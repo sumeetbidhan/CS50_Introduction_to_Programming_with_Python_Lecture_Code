255230230c9d773b9454581e701628e5b7f5e82b
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-06-26 17:39:27 +0530

    new

---
+++ libraries/name1.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+'''for arg in sys.argv:
+    print("hello, my name is", arg)'''
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
+
+for arg in sys.argv[1:-1]:
+    print("hello, my name is", arg)
+
+
